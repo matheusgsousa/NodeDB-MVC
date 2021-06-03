084a37c9c849e7850d9b6b0f810bf5f7a414b0ef
Matheus Gonçalves <matheus.gsousa@hotmail.com>
2021-06-03 14:53:44 -0300

    Auth Token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,7 +5,11 @@ const authConfig = require('../config/auth');
 const User = require('../models/User');
 
 const router = express.Router();
-
+function generateToken(params={}){
+  return jwt.sign(params, authConfig.secret,{
+    expiresIn: 86400,
+  });
+}
 router.post('/register', async(req, res)=>{
   const {email} = req.body;
   try{
@@ -14,7 +18,9 @@ router.post('/register', async(req, res)=>{
     
     const user = await User.create(req.body);
     user.password = undefined;
-    return res.send({user});
+    return res.send({user, 
+      token:generateToken({id:user.id}),
+    });
   }catch(err){
     return res.status(400).send({error: 'Registration failed'});
   }
@@ -31,10 +37,10 @@ router.post('/authenticate', async(req, res)=>{
     return res.status(400).send({error:'Invalid Password'});
   
   user.password = undefined;
-  const token = jwt.sign({id:user.id}, authConfig.secret,{
-    expiresIn:86400,
+
+  res.send({user, 
+    token:generateToken({id:user.id}),
   });
-  res.send({user, token});
 
 })
 module.exports = app => app.use('/auth', router);
